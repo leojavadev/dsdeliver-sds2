3eac954b774e4f319d38e11c22bc31466f26d9f6
leojavadev <leojavadev@gmail.com>
2021-01-10 19:28:50 -0300

    scrollWheelZoom enabled

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -73,7 +73,7 @@ export default function OrderLocation({ onChangeLocation }: Props) {
                     center={address.position}
                     zoom={13}
                     key={address.position.lat}
-                    scrollWheelZoom={false}
+                    scrollWheelZoom
                 >
                     <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> Contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                     <Marker position={address.position}>
